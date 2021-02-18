54cefba4c7e950c3a3a67cb0d2dabb66b80e5822
pulkit232 <79206130+pulkit232@users.noreply.github.com>
2021-02-18 11:19:06 +0530

    Add files via upload

---
+++ 5.use signedint exp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+/*************************************************************
+Author: pulkit choudhary
+Purpose: to read distance ( unsigned int ) and print it.
+*************************************************************/
+void main()
+{
+    unsigned int distance;
+    printf("enter the distance between two plantes:");
+    scanf("%u",&distance);
+    printf("you have entered : %u",distance);
+    getch();
+
+}
